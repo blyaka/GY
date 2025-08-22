f5e19ed119e0115de34c7212f25ef82e9f37f43f
Jigor <40729531+blyaka@users.noreply.github.com>
2025-08-22 17:38:15 +0300

    + delivery page

--- shop/pages/urls.py
+++ shop/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePage, ContactsPage
+from .views import HomePage, ContactsPage, DeliveryPage
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', HomePage, name='home'),
     path('contacts/', ContactsPage, name='contacts'),
+    path('delivery/', DeliveryPage, name='delivery')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
