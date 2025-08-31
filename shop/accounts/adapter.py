b3aa57e4c55f7f51b02390c27cdf5ee3c38885cb
Jigor <sloy425@gmail.com>
2025-08-31 16:20:06 +0300

    adapter

--- shop/accounts/adapter.py
+++ shop/accounts/adapter.py
@@ -1,4 +1,3 @@
-# accounts/adapter.py
 from allauth.account.adapter import DefaultAccountAdapter
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
@@ -6,20 +5,28 @@ from django.template import TemplateDoesNotExist
 from django.conf import settings
 
 class CustomAccountAdapter(DefaultAccountAdapter):
-    def send_mail(self, template_prefix, email, context):
+    def render_mail(self, template_prefix, email, context, headers=None):
         subject = render_to_string(f"{template_prefix}_subject.txt", context).strip()
-        try:
-            html = render_to_string(f"{template_prefix}_message.html", context)
-        except TemplateDoesNotExist:
-            html = None
-        try:
-            text = render_to_string(f"{template_prefix}_message.txt", context)
-        except TemplateDoesNotExist:
-            text = ""
+        subject = self.format_email_subject(subject)
 
-        msg = EmailMultiAlternatives(subject, html or text, settings.DEFAULT_FROM_EMAIL, [email])
-        if html:
+        html_body = None
+        text_body = None
+        try:
+            html_body = render_to_string(f"{template_prefix}_message.html", context)
+        except TemplateDoesNotExist:
+            pass
+        try:
+            text_body = render_to_string(f"{template_prefix}_message.txt", context)
+        except TemplateDoesNotExist:
+            pass
+
+        from_email = getattr(settings, "DEFAULT_FROM_EMAIL", None)
+
+        if html_body:
+            msg = EmailMultiAlternatives(subject, html_body, from_email, [email], headers=headers)
             msg.content_subtype = "html"
-            if text:
-                msg.attach_alternative(text, "text/plain")
-        msg.send()
+            if text_body:
+                msg.attach_alternative(text_body, "text/plain")
+        else:
+            msg = EmailMultiAlternatives(subject, text_body or "", from_email, [email], headers=headers)
+        return msg
