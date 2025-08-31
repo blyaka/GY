60da2ca574b9a7c8371e8a72667742b40dc54cff
Jigor <sloy425@gmail.com>
2025-08-31 16:07:56 +0300

    mail sys

--- shop/accounts/adapter.py
+++ shop/accounts/adapter.py
@@ -1,4 +1,4 @@
-
+# accounts/adapter.py
 from allauth.account.adapter import DefaultAccountAdapter
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
@@ -8,26 +8,18 @@ from django.conf import settings
 class CustomAccountAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         subject = render_to_string(f"{template_prefix}_subject.txt", context).strip()
-
-        html_body = None
-        text_body = None
         try:
-            html_body = render_to_string(f"{template_prefix}_message.html", context)
+            html = render_to_string(f"{template_prefix}_message.html", context)
         except TemplateDoesNotExist:
-            pass
+            html = None
         try:
-            text_body = render_to_string(f"{template_prefix}_message.txt", context)
+            text = render_to_string(f"{template_prefix}_message.txt", context)
         except TemplateDoesNotExist:
-            pass
+            text = ""
 
-        from_email = getattr(settings, "DEFAULT_FROM_EMAIL", None)
-
-        if html_body:
-            msg = EmailMultiAlternatives(subject, html_body, from_email, [email])
+        msg = EmailMultiAlternatives(subject, html or text, settings.DEFAULT_FROM_EMAIL, [email])
+        if html:
             msg.content_subtype = "html"
-            if text_body:
-                msg.attach_alternative(text_body, "text/plain")
-        else:
-            msg = EmailMultiAlternatives(subject, text_body or "", from_email, [email])
-
+            if text:
+                msg.attach_alternative(text, "text/plain")
         msg.send()
