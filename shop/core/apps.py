15f92d8bed7973ced0a6fbe5f1dc9a2e036d03c8
Jigor <sloy425@gmail.com>
2025-09-02 02:25:36 +0300

    update apps.py

--- shop/core/apps.py
+++ shop/core/apps.py
@@ -5,10 +5,12 @@ class CoreConfig(AppConfig):
     name = "core"
 
     def ready(self):
-        from django.db.models.signals import post_save
+        from django.db.models.signals import post_save, post_delete
         from django.core.cache import cache
         from .models import SiteContacts
 
         def clear_cache(*a, **kw):
             cache.delete("site_contacts")
+
         post_save.connect(clear_cache, sender=SiteContacts)
+        post_delete.connect(clear_cache, sender=SiteContacts)
