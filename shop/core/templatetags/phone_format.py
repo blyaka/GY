d236b78312f64f849ccf2acd965aa39ae485b559
jigor <40729531+blyaka@users.noreply.github.com>
2025-09-02 14:09:25 +0300

    Update phone_format.py

--- shop/core/templatetags/phone_format.py
+++ shop/core/templatetags/phone_format.py
@@ -6,8 +6,8 @@ register = template.Library()
 @register.filter
 def pretty_phone(value):
     """
-    Форматирует строку в вид +X XXX XXX XX XX
-    Пример: +79991234567 -> +7 999 123 45 67
+    Форматирует строку в вид +7 (XXX)-XXX-XX-XX
+    Пример: +79976848831 -> +7 (977)-684-88-31
     """
     if not value:
         return ''
@@ -17,6 +17,6 @@ def pretty_phone(value):
     if not digits.startswith('7'):
         digits = '7' + digits
     try:
-        return f'+{digits[0]} {digits[1:4]} {digits[4:7]} {digits[7:9]} {digits[9:11]}'
+        return f'+{digits[0]} ({digits[1:4]})-{digits[4:7]}-{digits[7:9]}-{digits[9:11]}'
     except IndexError:
         return '+' + digits
